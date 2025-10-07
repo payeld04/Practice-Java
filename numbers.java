b01547d5861fa592b4e3ed35da2ca8d10ba3d8f7
Payel <daspayell346@gmail.com>
2025-10-07 22:18:57 +0530

    problem 4

---
+++ numbers.java
@@ -0,0 +1,13 @@
+public class numbers
+{
+	public static void main(String[] args) 
+	{ 
+	    for(int i=1;i<=20;i++)
+	    {
+	        if(i<20)
+		System.out.print(i+",");
+		else
+		System.out.print(i);
+	    }
+	}
+}
