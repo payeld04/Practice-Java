03f554c0150d3016ecede7c8869297a61042e357
Payel <daspayell346@gmail.com>
2025-10-07 22:23:29 +0530

    problem 6

---
+++ TaxiChargeCalculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TaxiChargeCalculator
+{
+	public static void main(String[] args) 
+	{ 
+	    Scanner Sc= new Scanner(System.in);
+	    
+		System.out.println("Enter Taxi No.:");
+		String taxinumber= Sc.nextLine();
+		
+		System.out.print("Enter the distance covered(in km): ");
+		double distance= Sc.nextDouble();
+	}
+}
